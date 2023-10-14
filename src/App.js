63a2242f11d723a8f3d586e0e771221f2c22ed25
Umesh <umeshmane575@gmail.com>
2023-10-14 16:03:21 +0530

    i have created folder pages->auth and then file like home login and register imported browserrouter in the top index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Switch , Route }  from "react-router-dom"
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+
+
+
 
 function App() {
   return (
-    <div >
-    <p>react app</p>  
-    </div>
+    <Switch>
+      <Route exact path = "/" component = {Home}></Route>
+      <Route exact path = "/Login" component = {Login}></Route>
+      <Route exact  path = "/Register" component = {Register}></Route>
+    </Switch>
   );
 }
 
 export default App;
+
+
