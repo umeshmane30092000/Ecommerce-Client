88ae554abec9fa5c4327676a23c8f745b524e0d1
Umesh <umeshmane575@gmail.com>
2023-10-16 17:41:17 +0530

    We have created Header and made it clickable with the help of the react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Switch , Route }  from "react-router-dom"
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
-
+import Header from "./components/nav/Header";
 
 function App() {
   return (
+    <>
+    <Header/>
     <Switch>
       <Route exact path = "/" component = {Home}></Route>
       <Route exact path = "/Login" component = {Login}></Route>
       <Route exact  path = "/Register" component = {Register}></Route>
     </Switch>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import "antd/dist/antd.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter> ,
    
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
